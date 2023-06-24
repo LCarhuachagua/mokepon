904232e00eb004227ab1e58162398ef459c7d94f
Laly Carhuachagua <roselyn199206@gmail.com>
2023-06-24 01:24:36 -0500

    seleccionando mokepones con condicional

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,5 +1,22 @@
 function seleccionarMascotaJugador(){
-    alert('Seleccionaste tu mascota')
+    let radiobtnHipodoge = document.getElementById('Hipodoge').checked
+    let radiobtnCapipepo = document.getElementById('Capipepo').checked
+    let radiobtnRatigueya = document.getElementById('Ratigueya').checked
+    let radiobtnLangostelvis = document.getElementById('Langostelvis').checked
+    let radiobtnPydos = document.getElementById('Pydos').checked
+    let mokeponSeleccionado = ""
+    if (radiobtnHipodoge){
+        mokeponSeleccionado = "Hipodoge"
+    }else if (radiobtnCapipepo){
+        mokeponSeleccionado = "Capipepo"
+    }else if (radiobtnRatigueya){
+        mokeponSeleccionado = "Ratigueya"
+    }else if (radiobtnLangostelvis){
+        mokeponSeleccionado = "Langostelvis"
+    }else if (radiobtnPydos){
+        mokeponSeleccionado = "Pydos"
+    }
+    alert('Seleccionaste tu mascota: '+ mokeponSeleccionado)
 }
 function iniciarJuego(){
     let btnMascota = document.getElementById('boton-mascota')
